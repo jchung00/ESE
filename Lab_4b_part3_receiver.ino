955c14609fbfd9ee044e35f885b891aff8663623
Jae Chung <jae.chung99@gmail.com>
2018-10-15 15:50:17 -0400

    Update Lab_4b_part3_receiver.ino

--- Lab_4b_part3_receiver.ino
+++ Lab_4b_part3_receiver.ino
@@ -3,6 +3,7 @@ SoftwareSerial BTSerial(2, 3); //RX TX
 String outString =""; //this stores incoming string from the master
 boolean stringComplete = false; //to indicate when the string is completed
 const int LED = 8;
+int val = 0;
 
 void setup() {
   BTSerial.begin(9600); //begin bluetooth communication
@@ -20,10 +21,14 @@ void loop() {
     }
   }
   if (stringComplete) {
-    if(outString.equals("H")){
+    Serial.println(outString);
+    val = outString.toInt();
+    if(val == 1){
+      Serial.println("case 1");
       digitalWrite(LED, HIGH);
     }
     else{
+      Serial.println("case 2");
       digitalWrite(LED, LOW);
     }
     outString = ""; //clear this string to store new characters
